9d550988393baec8d0238bdbb37535bad8bbeeed
marinaKuschenkoo <marinakuschenko1@yandex.ru>
2023-09-09 04:38:04 +0300

    cors error

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,25 +1,29 @@
 const allowedCors = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'http://localhost:3001',
-  'https://localhost:3001',
+  "localhost:3000",
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "http://localhost:3001",
+  "https://localhost:3001",
+  " http://api.marina.k-diplom.nomoreparties.co",
+  " https://api.marina.k-diplom.nomoreparties.co",
+  "https://marina.k-diplom.nomoredomainsicu.ru",
+  "http://marina.k-diplom.nomoredomainsicu.ru",
 ];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers["access-control-request-headers"];
 
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
   }
 
